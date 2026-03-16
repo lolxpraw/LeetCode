61ac866b4579dd23d7b779bd53f5efc50cc7e255
lolxpraw <huuducnguyen2510@gmail.com>
2026-03-16 23:14:13 +0700

    Time: 0 ms (100.00%) | Memory: 11.6 MB (88.87%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        if(ransomNote.length() > magazine.length()) return false;
+        int charCount[26] = {0};
+        
+        for (char c : magazine) {
+            charCount[c - 'a']++; 
+        }
+        for (char c : ransomNote) {
+            if (charCount[c - 'a'] == 0) {
+                return false; 
+            }
+            charCount[c - 'a']--;
+        }
+        return true;
+    }
+};
