8275d59ed725aacba9af364673dd7fd3dbd09f26
lolxpraw <huuducnguyen2510@gmail.com>
2026-03-16 23:14:41 +0700

    Time: 0 ms (100.00%) | Memory: 113.9 MB (27.26%) - LeetSync

---
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        double sum = 0;
+        for(int i = 0;i < k;i++){
+            sum += nums[i];
+        } // Tinh tong cua k mang dau
+        double maxSum = sum;
+        for(int i = k;i < nums.size();i++){
+            sum += nums[i] - nums[i - k];
+            maxSum = max(maxSum, sum);
+        } // Tinh tong cua mang tiep theo , bo phan tu dau tien
+        return maxSum / k;
+    }
+};
