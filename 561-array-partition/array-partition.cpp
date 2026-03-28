0e2222a15f95783ea9915b302c1d1d087203d9b6
lolxpraw <huuducnguyen2510@gmail.com>
2026-03-28 09:05:46 +0700

    Time: 7 ms (74.93%) | Memory: 32.1 MB (56.23%) - LeetSync

--- 561-array-partition/array-partition.cpp
+++ 561-array-partition/array-partition.cpp
@@ -4,9 +4,10 @@
 class Solution {
 public:
     int arrayPairSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(),nums.end()); // mang sort
 
         int sum = 0;
+        // vi du mang da sort [3,6,8,9,11,15], vong lap for lay cac so 3,8,11 sau do cong lai (min)
         for(int i = 0;i < nums.size();i += 2){
             sum += nums[i]; 
         }
