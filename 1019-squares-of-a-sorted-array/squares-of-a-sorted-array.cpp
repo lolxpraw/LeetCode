eba5b0a3028a826536b1c77c5412eca9d80a046e
lolxpraw <huuducnguyen2510@gmail.com>
2026-03-28 10:05:16 +0700

    Time: 3 ms (53.69%) | Memory: 30.2 MB (64.78%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
         int n = nums.size(); // đặt biến thành kích thước của mảng
-        vector<int> kq(n); // tạo một mảng mới 
 
         for(int i = 0;i < n;i++){
             nums[i] = abs(nums[i]) * abs(nums[i]);
