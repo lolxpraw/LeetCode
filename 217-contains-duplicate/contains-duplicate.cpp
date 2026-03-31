a535c5bb70d1bdea5a8b64056984ed69632dfb70
lolxpraw <huuducnguyen2510@gmail.com>
2026-03-31 20:58:29 +0700

    Time: 16 ms (97.07%) | Memory: 71.1 MB (78.73%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int count = 1;
+        int n = nums.size();
+        
+        for(int i = 0;i < n - 1;i++){
+            if(nums[i] == nums[i + 1]){
+                count++;
+                if(count == 2) return true;
+            } 
+        }
+        return false;
+    }
+};
