973f13efcbc6386826574a69991e3ccc84a07edf
lolxpraw <huuducnguyen2510@gmail.com>
2026-04-03 09:36:49 +0700

    Time: 0 ms (100.00%) | Memory: 13.3 MB (97.48%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -5,12 +5,9 @@ public:
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (i != j && arr[i] == 2 * arr[j]) {
-                    return true;
-                }
+                if (i != j && arr[i] == 2 * arr[j]) return true;
             }
         }
-
         return false;
     }
 };
