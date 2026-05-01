a7a9c5ea4a6391c45838cfcf0b9b8fced5700700
lolxpraw <huuducnguyen2510@gmail.com>
2026-05-01 12:02:05 +0700

    Time: 0 ms (100.00%) | Memory: 146.7 MB (90.60%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
@@ -5,11 +5,14 @@ public:
     int minDepth(TreeNode* root) {
         // Th1:  cay rong
         if (root == nullptr) return 0;
+        // Th2 : 2 cay khong co con
         if (root->left == nullptr && root->right == nullptr) return 1;
+        // Th3: cay trai khong tim thay
         if (root->left == nullptr) return minDepth(root->right) + 1;
+        // Th4: cay phai khong tim thay
         if (root->right == nullptr) return minDepth(root->left) + 1;
 
+        //Th5: tong quat
         return min(minDepth(root->right), minDepth(root->left)) + 1;
-
     }
 };
