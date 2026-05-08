be2f5b13c35555e5e1ac9bcff71501fad9663a0e
lolxpraw <huuducnguyen2510@gmail.com>
2026-05-08 21:07:52 +0700

    Time: 12 ms (64.23%) | Memory: 14.5 MB (71.24%) - LeetSync

--- 774-maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.cpp
+++ 774-maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.cpp
@@ -1,34 +1,15 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    vector<Node*> children;
-
-    Node() {}
-
-    Node(int _val) {
-        val = _val;
-    }
-
-    Node(int _val, vector<Node*> _children) {
-        val = _val;
-        children = _children;
-    }
-};
-*/
-
+// Bai toan su dung de quy DFS
+// khoi tao 
 class Solution {
 public:
     int maxDepth(Node* root) {
         if (root == nullptr) {
             return 0;
         }
-        
-        int max_child_depth = 0;
+        int dfs = 0;
         for (Node* child : root->children) {
-            max_child_depth = max(max_child_depth, maxDepth(child));
+            dfs = max(dfs, maxDepth(child));
         }
-        return max_child_depth + 1;
+        return dfs + 1;
     }
 };
