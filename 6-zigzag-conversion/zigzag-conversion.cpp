fd8287b1bac450432ec1553e143e488579ff9510
Duc Nguyen Huu <huuducnguyen2510@gmail.com>
2026-09-05 16:20:14 +0700

    Time: 0 ms (100.00%) | Memory: 11.2 MB (86.90%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -0,0 +1,23 @@
+// theo pattern, vd: numRows = 3 => 123212321.....
+// numRows = 4 => 1234321234321............
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows == 1 || numRows >= s.length()) return s;
+        string row[numRows]; // ex: numRows = 3 => row[0],row[1],row[2]
+        int curRow = 0;
+        bool goingDown = true;
+        for(char c : s){
+            row[curRow] += c;
+            if(curRow == 0) goingDown = true;
+            if(curRow == numRows - 1) goingDown = false;
+            if(goingDown == true) curRow++;
+            else curRow--;
+        }
+    string ans = "";
+    for(int i = 0;i < numRows;i++){
+        ans += row[i];
+    }
+    return ans;
+    }
+};  
